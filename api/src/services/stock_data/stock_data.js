5539b2d4a8899df0ef18e7806b33c5dfdb23d290
Suraj Shrestha <syuraj@gmail.com>
2020-07-22 19:49:13 -0400

    Squashed commit of the following:
    commit 6180029bbc195192ef6a2352dcdc2578c2bacc66
    Author: Suraj Shrestha <syuraj@gmail.com>
    Date:   Wed Jul 22 19:15:28 2020 -0400
    
        remove redwood api and add apollo serverless and deploy to aws
    
    commit 19ad36530b520627dabcd62843d7d08780a8d099
    Author: Suraj Shrestha <syuraj@gmail.com>
    Date:   Wed Jul 22 07:57:10 2020 -0400
    
        readme update for aws deployment using github action
    
    commit af2eb55b5198e22b1d5d4f12394d07e8c5c08a3c
    Author: Suraj Shrestha <syuraj@gmail.com>
    Date:   Mon Jul 20 16:13:17 2020 -0400
    
        serverless.yml file format fix
    
    commit 5056b13e5ee9a9045fa1cab0044f606a39595395
    Author: Suraj Shrestha <syuraj@gmail.com>
    Date:   Mon Jul 20 16:02:57 2020 -0400
    
        s3 bucket configuration for static site
    
    commit b3d4fed771117a80c58272d000fd00961fc11499
    Author: Suraj Shrestha <syuraj@gmail.com>
    Date:   Sat Jul 18 16:46:54 2020 -0400
    
        Update serverless.yml
    
    commit e55f93d6774ac04c481336625b459406063729e6
    Author: Suraj Shrestha <syuraj@gmail.com>
    Date:   Sat Jul 18 16:38:55 2020 -0400
    
        minor path fix
    
    commit 1d111b67f2fdcb3d781b26e968318238914ef6eb
    Author: Suraj Shrestha <syuraj@gmail.com>
    Date:   Sat Jul 18 16:28:12 2020 -0400
    
        serverless deploy
    
    commit 26c1b7718cccaee6862e73097b0f64fc4edff278
    Author: Suraj Shrestha <syuraj@gmail.com>
    Date:   Sat Jul 18 13:04:17 2020 -0400
    
        Update amplify.yml
    
    commit afaa500f2bd9998123bb9c64ed4c2bd1b1add0e4
    Author: Suraj Shrestha <syuraj@gmail.com>
    Date:   Sat Jul 18 12:46:33 2020 -0400
    
        Update amplify.yml
    
    commit ebbe66c0f6d8ae9773a9ae225703f49b03105c2d
    Author: Suraj Shrestha <syuraj@gmail.com>
    Date:   Sat Jul 18 12:43:17 2020 -0400
    
        Update amplify.yml
    
    commit 10f9b85d418d91f5f2fe91045b67b5399fbe77d7
    Author: Suraj Shrestha <syuraj@gmail.com>
    Date:   Sat Jul 18 12:25:20 2020 -0400
    
        Update amplify.yml
    
    commit 591df0f5d34b9192e0919ed8d0161746f63eaa44
    Author: Suraj Shrestha <syuraj@gmail.com>
    Date:   Sat Jul 18 12:22:19 2020 -0400
    
        Update amplify.yml
    
    commit 7d88a7b6930442469563e81294cb839ad21d97b7
    Author: Suraj Shrestha <syuraj@gmail.com>
    Date:   Sat Jul 18 12:17:05 2020 -0400
    
        Update amplify.yml
    
    commit 5cf387f8c155d6826242dbdfe988174b73b75378
    Author: Suraj Shrestha <syuraj@gmail.com>
    Date:   Sat Jul 18 12:04:19 2020 -0400
    
        downgrading node version for amplify support

--- api/src/services/stock_data/stock_data.js
+++
@@ -1,5 +0,0 @@
-import { db } from 'src/lib/db'
-
-export const stock_data = () => {
-  return db.stock_data.findMany()
-}
--- api/src/services/stock_data/stock_data.test.js
+++
@@ -1,8 +0,0 @@
-import { stockData } from './stock_data'
-
-describe('stock data', () => {
-  it('returns stock data', () => {
-    stockData()
-    expect(true).toBe(true)
-  })
-})
